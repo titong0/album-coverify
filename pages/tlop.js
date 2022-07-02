00ba9bb544d791a1cc5748c7e66f368e3cefef3c
Titong0 <augustoerrecarte@gmail.com>
2022-07-01 23:59:33 -0300

    fix image preview

--- pages/tlop.js
+++ pages/tlop.js
@@ -5,7 +5,19 @@ const TLOP = () => {
   return (
     <>
       <Head>
-        <title>The life of pablo generator</title>
+        {/* <!-- HTML Meta Tags --> */}
+        <title>The life of pablo album cover generator</title>
+        <meta
+          name="description"
+          content="Turn your photos into a cover like the life of pablo"
+        />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta
+          property="og:url"
+          content="https://album-coverify.vercel.app/tlop/"
+        />
+        <meta property="og:type" content="website" />
         <meta
           property="og:title"
           content="The life of pablo album cover generator"
@@ -15,13 +27,37 @@ const TLOP = () => {
           content="Turn your photos into a cover like the life of pablo"
         />
         <meta
-          property="og:url"
-          content="https://album-coverify.vercel.app/tlop"
-        />
-        <meta
           property="og:image"
           content="https://album-coverify.vercel.app/TLOP.png"
         />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="album-coverify.vercel.app" />
+        <meta
+          property="twitter:url"
+          content="https://album-coverify.vercel.app/tlop/"
+        />
+        <meta
+          name="twitter:title"
+          content="The life of pablo album cover generator"
+        />
+        <meta
+          name="twitter:description"
+          content="Turn your photos into a cover like the life of pablo"
+        />
+        <meta
+          name="twitter:image"
+          content="https://album-coverify.vercel.app/TLOP.png"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://album-coverify.vercel.app/TLOP.png"
+        />
+
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:alt" content="Custom TLOP cover" />
+        {/* <!-- Meta Tags Generated via https://www.opengraph.xyz --> */}
       </Head>
 
       <div>
