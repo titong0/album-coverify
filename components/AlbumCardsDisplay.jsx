232b49b18c53197ac77b21c25fcb2b0af5040248
Titong0 <augustoerrecarte@gmail.com>
2022-07-03 20:12:58 -0300

    bigger cards

--- components/AlbumCardsDisplay.jsx
+++ components/AlbumCardsDisplay.jsx
@@ -18,7 +18,7 @@ const ALBUMS_INFO = [
 const AlbumCardsDisplay = () => {
   return (
     <div
-      className={`grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 sm:m-12 justify-center`}
+      className={`grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 sm:m-12 justify-center`}
     >
       {ALBUMS_INFO.map((ALBUM) => (
         <AlbumCard
