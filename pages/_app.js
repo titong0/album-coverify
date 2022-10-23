21180893e19d975321b1ac4e430ccf134a2a1a40
Titong0 <augustoerrecarte@gmail.com>
2022-10-23 18:36:42 -0300

    hopefully fix

--- pages/_app.js
+++ pages/_app.js
@@ -2,24 +2,28 @@ import NavBar from "../components/General/NavBar";
 import "../index.css";
 import "react-image-crop/dist/ReactCrop.css";
 import { useRouter } from "next/router";
-
+import Head from "next/head";
 const NO_AD_ROUTES = ["/contact", "/credits"];
 
 function MyApp({ Component, pageProps }) {
   const Router = useRouter();
   return (
     <>
+      <Head>
+        {NO_AD_ROUTES.includes(Router.asPath) ? (
+          console.log("NO ADS HERE")
+        ) : (
+          <script
+            async
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`}
+            crossOrigin="anonymous"
+          />
+        )}
+      </Head>
       <NavBar />
       <main>
         <Component {...pageProps} />
       </main>
-      {NO_AD_ROUTES.includes(Router.asPath) ? null : (
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1954485960736219"
-          crossOrigin="anonymous"
-        ></script>
-      )}
     </>
   );
 }
