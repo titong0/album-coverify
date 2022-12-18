fb50cd9c2747b77ec28e8c9383ef7552ee8e13ec
Titong0 <augustoerrecarte@gmail.com>
2022-12-17 22:48:26 -0300

    add wolf I think

--- components/Editors/WolfEditor.tsx
+++ components/Editors/WolfEditor.tsx
@@ -3,57 +3,66 @@ import Canvas from "../General/Canvas";
 import Download from "../General/Download";
 import EditorContainer from "../General/EditorContainer";
 import { Drawer } from "../../src/Drawer";
-import DetailedImageHandler from "../Inputs/DetailedImageHandler";
-import { DetailedImage } from "../../src/types";
-import TextInput from "../Inputs/TextInput";
 import SimpleImage from "../Inputs/SimpleImage";
+import { DetailedImage } from "../../src/types";
+import DetailedImageHandler from "../Inputs/DetailedImageHandler";
 
-const BlondeEditor = () => {
-  const [firstImage, setFirstImage] = useState("WOLF_DEFAULT_1.png");
-  const [secondImage, setSecondImage] = useState("WOLF_DEFAULT_2.png");
+const WolfEditor = () => {
+  const [firstImage, setFirstImage] = useState("/defaults/WOLF_DEFAULT_1.png");
+  const [secondImage, setSecondImage] = useState({
+    srcUrl: "/defaults/WOLF_DEFAULT_2.png",
+    opacity: 0.5,
+  });
 
   const draw = async (Ctx: Drawer) => {
     await Ctx.imgBg("/assets/WOLF_BG.png");
     await Ctx.drawFixedImage(
       firstImage,
       { x: 0, y: 1000 },
-      { width: 640, height: 830 },
+      { width: 600, height: 800 },
       { align: "bottom" }
     );
+    Ctx.utils.addFilter("opacity", secondImage.opacity);
+    await Ctx.drawFixedImage(
+      secondImage.srcUrl,
+      { x: 600, y: 500 },
+      { width: 320, height: 460 },
+      { align: "bottom" }
+    );
+    Ctx.utils.resetFilters();
   };
 
   return (
-    <EditorContainer drawMethod={draw} dependencies={[image, title]}>
+    <EditorContainer drawMethod={draw} dependencies={[firstImage, secondImage]}>
       <div className="sm:grid grid-cols-2 mb-32">
         <div className="flex flex-col">
           <SimpleImage
             label="First (big) image"
             name="firstImage"
-            aspect={640 / 830}
+            aspect={600 / 800}
             srcUrl={firstImage}
             setSrcUrl={setFirstImage}
           />
-          <SimpleImage
+          <DetailedImageHandler
             label="Second (small) image"
             name="secondImage"
             aspect={320 / 460}
-            srcUrl={firstImage}
-            setSrcUrl={setFirstImage}
-          />
+            image={secondImage}
+            setImage={setSecondImage}
+          >
+            <DetailedImageHandler.OpacityHandler />
+          </DetailedImageHandler>
         </div>
         <Canvas />
       </div>
       <Download
-        fileName="BLONDE"
+        fileName="WOLF"
+        title=""
         buttonStyle="text-black rounded-sm bg-green-500"
         bg="bg-green-900"
       />
     </EditorContainer>
   );
-};
-
-const WolfEditor = () => {
-  return <div>WolfEditor</div>;
 };
 
 export default WolfEditor;
