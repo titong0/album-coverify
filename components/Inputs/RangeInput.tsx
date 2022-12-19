99d3f0b274dc731243b7ee008fedecfa13221f59
tito-depto <augustoerrecarte@gmail.com>
2022-12-19 20:57:56 -0300

    add margin and possibility to give class to container

--- components/Inputs/RangeInput.tsx
+++ components/Inputs/RangeInput.tsx
@@ -4,10 +4,12 @@ import { InputProps, stateSetter } from "../../src/types";
 type RangeInputProps = InputProps & {
   name?: string;
   label: string | React.ReactNode;
+  className?: string;
   value: number;
   setValue: (arg0: number) => void;
 };
 const RangeInput: React.FC<RangeInputProps> = ({
+  className,
   value,
   setValue,
   name,
@@ -15,12 +17,12 @@ const RangeInput: React.FC<RangeInputProps> = ({
   ...inputProps
 }) => {
   return (
-    <div className="flex flex-col mt-2">
+    <div className={"flex flex-col mt-2 " + className}>
       <label htmlFor={name || "range"}>{label}</label>
       <input
         name={name || "range"}
         type="range"
-        className="sm:w-2/3"
+        className="sm:w-2/3 mt-2"
         onChange={(e) => setValue(parseFloat(e.target.value))}
         value={value}
         {...inputProps}
